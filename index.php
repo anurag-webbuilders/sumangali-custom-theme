665b76eddfb33ff4d5cc9ade32a8b7d04b9b1418
anurag-webbuilders <anuragwebbuilders@gmail.com>
2026-01-06 15:04:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php get_header(); ?>
 
+<div class="container">
+    <h1 class="premium-title">Our Exclusive Saari Collection</h1>
+    
+    <?php 
+    // This shortcode automatically pulls the newest products the admin added
+    echo do_shortcode('[products limit="8" columns="4" orderby="date"]'); 
+    ?>
+</div>
+
+<?php get_footer(); ?>
